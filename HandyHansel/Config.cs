677046afa1396904f8c42591956596aa61ff5429
John Marsden <jmm15f@acu.edu>
2020-05-11 18:56:30 -0500

    Add project files.

---
+++ HandyHansel/Config.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Enums;
+using System;
+
+namespace HandyHansel
+{
+    internal class Config
+    {
+        internal Config()
+        {
+            ClientConfig = new DiscordConfiguration
+            {
+                Token = "BOT_TOKEN",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug,
+            };
+
+            CommandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { "^" },
+            };
+
+            InteractivityConfig = new InteractivityConfiguration
+            {
+                PaginationBehaviour = PaginationBehaviour.Ignore,
+                Timeout = TimeSpan.FromMinutes(2),
+            };
+        }
+
+        internal DiscordConfiguration ClientConfig { get; set; }
+        internal CommandsNextConfiguration CommandsConfig { get; set; }
+        internal InteractivityConfiguration InteractivityConfig { get; set; }
+    }
+}
