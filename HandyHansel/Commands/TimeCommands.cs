8d3631b30d4a2722c01f943a0111b7b7b56b6ea4
John Marsden <jmm15f@acu.edu>
2020-07-19 17:18:03 -0500

    Rewrite to use singletons

--- HandyHansel/Commands/TimeCommands.cs
+++ HandyHansel/Commands/TimeCommands.cs
@@ -13,86 +13,103 @@ using System.Threading.Tasks;
 
 namespace HandyHansel.Commands
 {
-    [Group("time"), Description("All commands associated with current time functionalities.")]
+    [Group("time"), Description("All commands associated with current time functionality.")]
     public class TimeCommands : BaseCommandModule
     {
-        private static readonly List<TimeZoneInfo> systemTimeZones = TimeZoneInfo.GetSystemTimeZones().ToList();
+        // The list of all system time zones
+        private readonly List<TimeZoneInfo> _systemTimeZones = TimeZoneInfo.GetSystemTimeZones().ToList();
 
-        public PostgreSqlContext DbContext { get; }
-        public IDataAccessProvider DataAccessProvider { get; }
+        // Singleton of systemTimeZonePages
+        private Page[] _systemTimeZonePages = null;
+
+        private IEnumerable<Page> GetSystemTimeZonePages(InteractivityExtension interactivity)
+        {
+            // If the singleton is not null then we should just return it.
+            if (!(_systemTimeZonePages is null)) return _systemTimeZonePages;
+
+            string allPossibleTimesString = "";
+            for (int i = 0; i < _systemTimeZones.Count; i++)
+            {
+                allPossibleTimesString += $"{i + 1}. {_systemTimeZones[i].Id}\n";
+            }
+
+            DiscordEmbedBuilder baseDiscordEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Choose a time!",
+            };
+
+            _systemTimeZonePages =
+                interactivity.GeneratePagesInEmbed(allPossibleTimesString, SplitType.Line, baseDiscordEmbed);
+
+            return _systemTimeZonePages;
+        }
+
+        // Singleton map of Guild Time Zone Infos
+        
+        
+        // Singleton map of Guild Times
+        private readonly Dictionary<ulong, Page[]> _guildTimeZonePages;
+
+        private IEnumerable<Page> GetGuildTimeZonePages(ulong guildId, InteractivityExtension interactivity)
+        {
+            if (_guildTimeZonePages.ContainsKey(guildId)) return _guildTimeZonePages[guildId];
+            string description = "";
+            int count = 1;
+            List<string> guildAssociatedTimeZones = DataAccessProvider.GetAllAssociatedGuildTimeZones(guildId.ToString()).Select(gtz => gtz.TimeZoneId).ToList();
+            foreach (TimeZoneInfo timezone in _systemTimeZones.Where(timezone => guildAssociatedTimeZones.Contains(timezone.Id)))
+            {
+                description += $"{count}. {timezone.Id}\n";
+                count++;
+            }
+            
+            _guildTimeZonePages[guildId] = interactivity.GeneratePagesInEmbed(description, SplitType.Line);
+            return _guildTimeZonePages[guildId];
+        }
+        
+
+        private IDataAccessProvider DataAccessProvider { get; }
 
         public TimeCommands(PostgreSqlContext sqlContext, IDataAccessProvider dataAccessProvider)
         {
-            DbContext = sqlContext;
             DataAccessProvider = dataAccessProvider;
+            _guildTimeZonePages = new Dictionary<ulong, Page[]>();
         }
-
-        private static bool currentlyPresentingGuildTimes = false;
 
         [GroupCommand, Description("Allows a user to select a time zone and Handy Hansel will say what time it is there.")]
         public async Task ExecuteGroupAsync(CommandContext context)
         {
-            if (!currentlyPresentingGuildTimes)
-            {
-                await context.RespondAsync($":wave: Hi, {context.User.Mention}! What timezone do you want the time for?");
-                List<TimeZoneInfo> associatedTimeZones = getListOfTimeZones(context.Guild.Id.ToString());
-                InteractivityExtension interactivity = context.Client.GetInteractivity();
-                _ = PresentAssociatedTimeZones(associatedTimeZones, interactivity, context.Channel, context.User);
-                await context.RespondAsync($"Choose a timezone by typing: ^time <timezone number>");
-                InteractivityResult<DiscordMessage> result = await interactivity.WaitForMessageAsync(xm => xm.Content.StartsWith("^time"), timeoutoverride: TimeSpan.FromMinutes(5));
+            await context.RespondAsync($":wave: Hi, {context.User.Mention}! What timezone do you want the time for?");
+            InteractivityExtension interactivity = context.Client.GetInteractivity();
+            _ = interactivity.SendPaginatedMessageAsync(context.Channel, context.User, GetGuildTimeZonePages(context.Guild.Id, interactivity), behaviour: PaginationBehaviour.WrapAround, timeoutoverride: TimeSpan.FromMinutes(5));
+            await context.RespondAsync($"Choose a timezone by typing: <timezone number>");
+            InteractivityResult<DiscordMessage> result = await interactivity.WaitForMessageAsync(xm => int.TryParse(xm.Content, out _) && xm.Author.Equals(context.User), timeoutoverride: TimeSpan.FromMinutes(5));
 
-                if (!result.TimedOut)
+            if (!result.TimedOut)
+            {
+                DateTime currentTime = DateTime.Now;
+                TimeZoneInfo requestedTimeZone = GetListOfTimeZones(context.Guild.Id)[(int.Parse(result.Result.Content) - 1)];
+                DateTime requestedTime = TimeZoneInfo.ConvertTime(currentTime, TimeZoneInfo.Local, requestedTimeZone);
+                DiscordEmbed timeEmbed = new DiscordEmbedBuilder
                 {
-                    DateTime currentTime = DateTime.Now;
-                    TimeZoneInfo requestedTimeZone = associatedTimeZones[(int.Parse(result.Result.Content.Substring("^time".Length)) - 1)];
-                    DateTime requestedTime = TimeZoneInfo.ConvertTime(currentTime, TimeZoneInfo.Local, requestedTimeZone);
-                    DiscordEmbed timeEmbed = new DiscordEmbedBuilder
-                    {
-                        Title = "Current Time: " + requestedTimeZone.Id,
-                        Description = $"{requestedTime}",
-                    };
-                    await context.RespondAsync(embed: timeEmbed);
-                }
+                    Title = "Current Time: " + requestedTimeZone.Id,
+                    Description = $"{requestedTime}",
+                };
+                await context.RespondAsync(embed: timeEmbed);
             }
-            else
-            {
-                await context.RespondAsync($"Before you attempt to ask for another time please press the :stop_button: button on the embed above.");
-            }
+
         }
-
-        /// <summary>
-        /// This presents the associated timezones asynchronously so that the rest of the code can run synchronously.
-        /// </summary>
-        /// <param name="associatedTimeZones">The list of timezones to present.</param>
-        /// <param name="interactivity">The interactivity extension to be used.</param>
-        /// <param name="channel">The DiscordChannel to present to.</param>
-        /// <param name="user">The DiscordUser to expect responses from.</param>
-        /// <returns></returns>
-        private async Task PresentAssociatedTimeZones(List<TimeZoneInfo> associatedTimeZones, InteractivityExtension interactivity, DiscordChannel channel, DiscordUser user)
-        {
-            currentlyPresentingGuildTimes = true;
-
-            string description = "";
-            for (int i = 0; i < associatedTimeZones.Count; i++)
-            {
-                description += $"{i + 1}. {associatedTimeZones[i].Id}\n";
-            }
-
-            Page[] correctResponsesPages = interactivity.GeneratePagesInEmbed(description, SplitType.Line);
-            await interactivity.SendPaginatedMessageAsync(channel, user, correctResponsesPages, behaviour: PaginationBehaviour.WrapAround, timeoutoverride: TimeSpan.FromMinutes(5));
-            currentlyPresentingGuildTimes = false;
-        }
+        
 
         /// <summary>
         /// Get list of all guild time zones as a list of TimeZoneInfo's that can then be used to calculate times.
         /// </summary>
         /// <param name="guildId">The guild id of the guild time zones we are pulling</param>
         /// <returns>The list of Time Zone Infos associated with the passed guild id.</returns>
-        List<TimeZoneInfo> getListOfTimeZones(string guildId)
+        List<TimeZoneInfo> GetListOfTimeZones(ulong guildId)
         {
             List<TimeZoneInfo> timeZoneInfos = new List<TimeZoneInfo>();
-            List<string> guildAssociatedTimeZones = DataAccessProvider.GetAllAssociatedGuildTimeZones(guildId).Select(gtz => gtz.TimeZoneId).ToList();
-            foreach (TimeZoneInfo timezone in systemTimeZones)
+            List<string> guildAssociatedTimeZones = DataAccessProvider.GetAllAssociatedGuildTimeZones(guildId.ToString()).Select(gtz => gtz.TimeZoneId).ToList();
+            foreach (TimeZoneInfo timezone in _systemTimeZones)
             {
                 if (guildAssociatedTimeZones.Contains(timezone.Id))
                     timeZoneInfos.Add(timezone);
@@ -100,28 +117,23 @@ namespace HandyHansel.Commands
             return timeZoneInfos;
         }
 
-        private static bool currentlyPresentingSystemTimes = false;
-
-        [Command("addTime"), Description("Allows a user to add a new time zone to the time zone options for the guild")]
+        [Command("addtime"), Description("Allows a user to add a new time zone to the time zone options for the guild")]
         public async Task AddTimeZoneToDb(CommandContext context)
         {
             try
             {
                 InteractivityExtension interactivity = context.Client.GetInteractivity();
-
-                if (!currentlyPresentingSystemTimes)
-                {
-                    _ = PresentSystemTimes(interactivity, context.Channel, context.User);
-                }
-                await context.RespondAsync($"Choose a timezone by typing: ^addtime <timezone number>");
-                InteractivityResult<DiscordMessage> msg = await interactivity.WaitForMessageAsync(xm => xm.Content.StartsWith("^addtime"), timeoutoverride: TimeSpan.FromMinutes(5));
+                
+                _ = interactivity.SendPaginatedMessageAsync(context.Channel, context.User, GetSystemTimeZonePages(interactivity), behaviour: PaginationBehaviour.WrapAround, timeoutoverride: TimeSpan.FromMinutes(5));
+                await context.RespondAsync($"Choose a timezone by typing: <timezone number>");
+                InteractivityResult<DiscordMessage> msg = await interactivity.WaitForMessageAsync(xm => int.TryParse(xm.Content, out _), timeoutoverride: TimeSpan.FromMinutes(5));
                 
                 if (!msg.TimedOut)
                 {
-                    string operating_system = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+                    string operatingSystem = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
                     string guild = context.Guild.Id.ToString();
-                    string timeZoneId = systemTimeZones[(int.Parse(msg.Result.Content.Substring("^addtime".Length))-1)].Id;
-                    GuildTimeZone newGuildTimeZone = new GuildTimeZone { Guild = guild, TimeZoneId = timeZoneId, OperatingSystem = operating_system };
+                    string timeZoneId = _systemTimeZones[(int.Parse(msg.Result.Content)-1)].Id;
+                    GuildTimeZone newGuildTimeZone = new GuildTimeZone { Guild = guild, TimeZoneId = timeZoneId, OperatingSystem = operatingSystem };
                     DataAccessProvider.AddGuildTimeZone(newGuildTimeZone);
                     await context.RespondAsync($"I added { timeZoneId } to the timezone options for this guild.");
                 } 
@@ -132,39 +144,13 @@ namespace HandyHansel.Commands
             }
         }
 
-        /// <summary>
-        /// Present the system times asynchronously in an embed.
-        /// </summary>
-        /// <param name="interactivity">The interactivity extension</param>
-        /// <param name="channel">The channel to post the message in</param>
-        /// <param name="user">The user to expect input from.</param>
-        /// <returns></returns>
-        private async Task PresentSystemTimes(InteractivityExtension interactivity, DiscordChannel channel, DiscordUser user)
-        {
-            string allPossibleTimesString = "";
-            for (int i = 0; i < systemTimeZones.Count; i++)
-            {
-                allPossibleTimesString += $"{i + 1}. {systemTimeZones[i].Id}\n";
-            }
-
-            DiscordEmbedBuilder baseDiscordEmbed = new DiscordEmbedBuilder
-            {
-                Title = "Choose a time!",
-            };
-
-            Page[] allPossibleTimesPages = interactivity.GeneratePagesInEmbed(allPossibleTimesString, SplitType.Line, baseDiscordEmbed);
-            currentlyPresentingSystemTimes = true;
-            await interactivity.SendPaginatedMessageAsync(channel, user, allPossibleTimesPages, behaviour: PaginationBehaviour.WrapAround, timeoutoverride: TimeSpan.FromMinutes(5));
-            currentlyPresentingSystemTimes = false;
-        }
-
         [Command("allTimes"), RequireUserPermissions(Permissions.Administrator), Hidden]
         public async Task AllTimesOnSystem(CommandContext context)
         {
             string description = "";
-            for (int i = 0; i < systemTimeZones.Count; i++)
+            for (int i = 0; i < _systemTimeZones.Count; i++)
             {
-                description += systemTimeZones[i].Id + (i == systemTimeZones.Count - 1 ? " " : "\n\n");
+                description += _systemTimeZones[i].Id + (i == _systemTimeZones.Count - 1 ? " " : "\n\n");
             }
 
             System.IO.File.WriteAllText(@"AllTimes.txt", description);
