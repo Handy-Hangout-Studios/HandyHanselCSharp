bf964da6d7d117399d18a39d2204b60d8437cc89
jmmarsde <jmmarsde@ncsu.edu>
2020-08-22 15:57:50 -0400

    Add Event Commands to Handy Hansel

---
+++ HandyHansel/Models/GuildEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandyHansel.Models
+{
+    [Table("all_guild_events")]
+    public class GuildEvent
+    {
+        [Key] 
+        [Column("id")] 
+        public int Id { get; set; }
+        
+        [Column("guild")] 
+        public ulong GuildId { get; set; }
+        
+        [Column("event_name")] 
+        public string EventName { get; set; }
+        
+        [Column("event_description")] 
+        public string EventDesc { get; set; }
+    }
+}
